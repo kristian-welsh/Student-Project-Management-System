8f216d16cc4f5a0f08ac0fb0b3f1df004fadb0ec
Kristian Welsh <kjw28@sussex.ac.uk>
2019-04-09 11:26:06 +0100

    build changed for some reason

--- Server/target/generated-sources/annotations/com/kjw28/server/entity/Project_.java
+++ Server/target/generated-sources/annotations/com/kjw28/server/entity/Project_.java
@@ -7,7 +7,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-04-08T14:04:15")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-04-09T11:20:49")
 @StaticMetamodel(Project.class)
 public class Project_ { 
 
--- Server/target/generated-sources/annotations/com/kjw28/server/entity/Supervisor_.java
+++ Server/target/generated-sources/annotations/com/kjw28/server/entity/Supervisor_.java
@@ -6,7 +6,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-04-08T14:04:15")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-04-09T11:20:49")
 @StaticMetamodel(Supervisor.class)
 public class Supervisor_ { 
 
